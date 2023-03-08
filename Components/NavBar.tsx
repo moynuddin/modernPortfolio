fcefb3b83b1df325da77bd0d085e025d02182456
Moin uddin <moin_uddin@mohawkind.com>
2023-03-08 23:52:05 +0530

    remove type safein navbar

--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -7,10 +7,10 @@ import Link from "next/link";
 import { Social } from "../typings";
 import { urlFor } from "../sanity";
 type Props = {
-  socials: Social[];
+  socials: any;
 };
 
-const NavBar = (socials: any) => {
+const NavBar = ({ social }: any) => {
   return (
     <header className="sticky top-0 flex flex-start justify-between p-5 max-w-5xl mx-auto z-20 xl:items-center">
       <motion.div
@@ -29,7 +29,7 @@ const NavBar = (socials: any) => {
         }}
         className="flex flex-row gap-3"
       >
-        {socials.social.map((s: any) => (
+        {social?.map((s: any) => (
           <Link href={s?.url} target="_blank" key={s._id}>
             <Image
               key={s._id}
