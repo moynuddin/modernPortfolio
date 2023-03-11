51353bfa8f5926694568d7c2e9a69f746dd1bb12
Moin uddin <moin_uddin@mohawkind.com>
2023-03-11 20:50:28 +0530

    change URL

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -1,7 +1,7 @@
 import { Skill, Social } from "../typings";
 
 export const getSocialData = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
+  const res = await fetch("https://localhost:3000/api/getSocials");
 
   const data = await res.json();
 
