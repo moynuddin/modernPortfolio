2feed7dbce85507c9b5e0d4e3734aa076e17a4d3
Moin uddin <moin_uddin@mohawkind.com>
2023-03-15 18:30:35 +0530

    sanity project id

--- sanity.ts
+++ sanity.ts
@@ -3,7 +3,8 @@ import imageUrlBuilder from "@sanity/image-url";
 
 export const config: any = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  // projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: "hsy4aenk",
   apiVersion: "2021-10-21",
   // useCdn: false,
   useCdn: process.env.NODE_ENV === "production",
