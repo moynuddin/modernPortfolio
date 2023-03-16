817adcd75e3cf58a2dd1489e24ce033decc21be1
Moin uddin <moin_uddin@mohawkind.com>
2023-03-16 14:38:09 +0530

    added back

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -9,7 +9,7 @@ export const getSocialData = async () => {
 };
 
 export const getSkillsData = async () => {
-  const res = await fetch("http://localhost:3000/api/getSkills");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
 
   const data = await res.json();
 
